841ace7171b09a9e9b71b46208928347f6fe0e88
kingchewie <seanmcquillen@gmail.com>
2023-10-08 14:57:23 -0400

    Update execute66.py
    adjusted to handle --gui pass

--- BigPythonEnergy/execute66.py
+++ BigPythonEnergy/execute66.py
@@ -3,4 +3,8 @@ import sys
 if str(len(sys.argv) > 1 and sys.argv[1]) == "--cli":
     exec(open("BigPythonEnergy/MainCLI.py").read())
 
+if str(len(sys.argv) > 1 and sys.argv[1]) == "--gui":
+    exec(open("BigPythonEnergy/MainUI.py").read())
+
+    
 exec(open("BigPythonEnergy/MainUI.py").read())
