608402c34f8f1e02ce15260350c9b5f91db3560c
kingchewie <seanmcquillen@gmail.com>
2023-10-08 15:08:16 -0400

    Update execute66.py
    adjusted to check platform gui support

--- BigPythonEnergy/execute66.py
+++ BigPythonEnergy/execute66.py
@@ -1,10 +1,35 @@
 import sys
+import platform
 
-if str(len(sys.argv) > 1 and sys.argv[1]) == "--cli":
-    exec(open("BigPythonEnergy/MainCLI.py").read())
+def is_gui_supported():
+    system = platform.system()
+    
+    if system == "Windows":
+        return True  # Windows generally supports GUI
+    elif system == "Linux":
+        # Check if X Window System (X11) is available
+        try:
+            import tkinter
+            tkinter.Tk()
+            return True
+        except ImportError:
+            return False
+    elif system == "Darwin":  # macOS
+        # Check for GUI environment (Aqua)
+        return "Aqua" in platform.mac_ver()[0]
+    
+    return False  # Unknown system or unsupported
 
-if str(len(sys.argv) > 1 and sys.argv[1]) == "--gui":
+if is_gui_supported():
+    if str(len(sys.argv) > 1 and sys.argv[1]) == "--cli":
+        exec(open("BigPythonEnergy/MainCLI.py").read())
+
+    if str(len(sys.argv) > 1 and sys.argv[1]) == "--gui":
+        exec(open("BigPythonEnergy/MainUI.py").read())
+
+
     exec(open("BigPythonEnergy/MainUI.py").read())
 
-    
-exec(open("BigPythonEnergy/MainUI.py").read())
+exec(open("BigPythonEnergy/MainCLI.py").read())
+
+
